763bceeec3e10e838e8d110a4ead16c5f87fc5d6
Christian Santos <christian@therubythree.com>
2019-05-08 16:40:08 -0700

    chapter 10 iteration e1

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -5,7 +5,7 @@ module CurrentCart #Created
    def set_cart                
     @cart = Cart.find(session[:cart_id])
      rescue ActiveRecord::RecordNotFound
-        @cart = Cart.Create
+        @cart = Cart.create
         session[:cart_id] = @cart.id
     end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,19 @@
 class Cart < ApplicationRecord
     has_many :line_items, dependent: :destroy
+
+    def add_product(product)
+        current_item = line_items.find_by(product_id: product.id)
+        if current_item
+            current_item.quantity += 1 
+        else 
+            current_item = line_items.build(product_id: product.id)
+        end
+        current_item
 end
+end
+
+# smart add_product() method in our Cart, one that checks if our 
+# list of items already includes the product we’re adding; if it does, it 
+# bumps the quantity, and if it doesn’t, it builds a new LineItem:
+# The find_by() method is a streamlined version of the where() method. 
+# Instead of returning an array of results, it returns either an existing LineItem or nil.
