27fe2fb2c733e87b2a2bd27848b2e9e003cf275a
Christian Santos <christian@therubythree.com>
2019-05-17 16:12:01 -0700

    Chapter 17 Deployment

--- Gemfile
+++ Gemfile
@@ -27,6 +27,10 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
  gem 'bcrypt', '~> 3.1.7'
 
+ # Translations into javascript object and allows us to access them
+
+gem 'i18n-js'
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,11 +1,15 @@
 class StoreController < ApplicationController
   skip_before_action :authorize
-
   include CurrentCart
   before_action :set_cart
+  
   def index
+    if params[:set_locale]
+      redirect_to store_index_url(local: params[:set_locale])
+    else
     @products = Product.order(:title) #We need to get the list of products out of the database and make it available to the code in the view that’ll display the table.
     # @product to ask the model for list of products in product.rb
     # (:title) displays products in alphabetical
+    end
   end
 end
