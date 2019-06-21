781d79e156a5fd0a9ea77458ba35907e7976430f
Christiansantoss <christian@therubythree.com>
2019-06-21 09:35:05 -0700

    small change

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   # validates :name, presence: true, uniqueness: true
   # has_secure_password
+  has_many :orders
 
   after_destroy :ensure_an_admin_remains
 
