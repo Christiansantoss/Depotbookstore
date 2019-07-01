f550646ce376f3a5485c720b6fac9e9cd00cb3bb
Christian santos <christian@therubythree.com>
2019-07-01 15:26:20 -0700

    issue with remote origin deleted comments to try and re push code to bitbucket

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -7,7 +7,3 @@ class LineItem < ApplicationRecord
   end
 end
 
-# Database now has a place to store the references among the line items, carts and products.
-# belongs_to() method defines and ancessor method carts and products
-# Tells Rails that Rows in line_items are the children of rows in carts and products
-# No line item can exist unless the corresponding cart and product rows exist 
