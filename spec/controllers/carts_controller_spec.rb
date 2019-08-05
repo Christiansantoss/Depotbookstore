d46aa05e8d7b5727b80e87b197c927c81acc9f93
Christian santos <christian@therubythree.com>
2019-08-05 16:38:38 -0700

    testing with rspec the carts controller for a response getting familair with testing tools, will be writing more tests tomorrow

---
+++ spec/controllers/carts_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe CartsController, type: :controller do
+  context 'GET #index' do
+    it 'returns a success response' do
+        get :index
+        expect(response).to be_success
+    end
+ end
+
+
+end 
