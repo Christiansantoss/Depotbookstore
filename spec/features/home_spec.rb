c3b5516e7c6225d56d65767454c7a4ffaad20af6
Christian santos <christian@therubythree.com>
2019-08-07 15:14:46 -0700

    changed home_spec.rb file added scenario test case for user clicking language to spanish

---
+++ spec/features/home_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+require 'byebug'
+
+
+feature "Visiting Homepage" do
+  scenario 'The visitor should see index page ', js: true do
+    visit root_path
+    expect(page).to have_text("Your Pragmatic Catalog")
+  end
+
+  scenario "The Visitor Clicks Language to Change locale to Spanish", js: true do
+    visit root_path
+    expect(page).to have_selector(:link_or_button, "Language")
+    click_link_or_button("Language")
+    click_link_or_button("Spanish")
+    expect(page).to have_text("Su Catálogo de Pragmatic")
+  end
+end
+
