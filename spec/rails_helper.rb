0073fb68248b47973e20dc96da5e05aed42721cc
Christian santos <christian@therubythree.com>
2019-08-16 14:59:18 -0700

    rails helper and config.yml

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -45,13 +45,13 @@ RSpec.configure do |config|
   end
 
 
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
+config.before(:each) do
+  DatabaseCleaner.strategy = :transaction
+end
 
-  config.before(:each, js: true) do
-    DatabaseCleaner.strategy = :truncation
-  end
+config.before(:each, js: true) do
+  DatabaseCleaner.strategy = :truncation
+end
 
   # This block must be here, do not combine with the other `before(:each)` block.
   # This makes it so Capybara can see the database.
